64aea90fef9f91443574a1e89bbb9b7568d6c82d
amine-am <aminealaouim98@gmail.com>
2022-02-04 16:35:09 +0100

    First commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into categories(designation) values('cat01');
+insert into categories(designation) values('cat02');
+insert into categories(designation) values('cat03');
+insert into categories(designation) values('cat04');
